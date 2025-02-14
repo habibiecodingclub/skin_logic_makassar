a532b9c41548aa8b8390b6f0a542149fdf029b07
Ameeeek <ameeeekwork@gmail.com>
2025-02-14 08:41:31 +0800

    [Ticket_6] Feat : add input field

---
+++ database/migrations/2025_02_13_105824_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('Order_List');
+            $table->integer("Price_per_Unit");
+            $table->integer('Total_Price');
+            $table->integer('Total_Payment');
+            $table->enum('Payment_Method', ['Cash', 'Transfer']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
